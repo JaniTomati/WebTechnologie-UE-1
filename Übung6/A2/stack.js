8bb269a0b139cfac7f6440fb3f898b357401abb0
Jula McGibbon <julamcgibbon@gmail.com>
2017-06-28 16:06:16 +0200

    Adding push/pop list task2

---
+++ Übung6/A2/stack.js
@@ -0,0 +1,22 @@
+function push() {
+
+	var li = document.createElement("li");
+	li.textContent = document.control.pushText.value;
+	document.getElementsByTagName("ul")[0].appendChild(li);
+ }
+
+ function pop() {
+
+ 	var elem_name = document.getElementsByTagName("ul")[0].lastChild.innerHTML;
+	document.getElementsByTagName("ul")[0].lastChild.remove();
+	document.control.popText.value = elem_name;
+ }
+
+function initialize(){
+
+	var push_  = document.getElementById('pushButton');
+	push_.addEventListener ('click', push);
+
+	var pop_  = document.getElementById('popButton');
+	pop_.addEventListener ('click', pop);
+}
